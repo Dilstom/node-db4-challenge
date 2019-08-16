7b1f11a46bea9e65ac4467ba47e2397b50dd9eae
Dil Sombayeva <dilyasom@gmail.com>
2019-08-16 16:46:56 -0700

    getShoppingList model

--- recipe/recipe-model.js
+++ recipe/recipe-model.js
@@ -5,6 +5,7 @@ module.exports = {
  add,
  findById,
  getInstructions,
+ getShoppingList,
 };
 
 function getRecipes() {
@@ -22,8 +23,16 @@ async function add(info) {
 
 function getInstructions(id) {
  return db('recipe as r')
-  .join('steps as s', 's.recipe_id', '=', 'r.id')
+  .join('steps as s', 's.recipe_id', 'r.id')
   .select('r.name', 'step_number', 's.description')
   .where('recipe_id', id)
   .orderBy('step_number', 'asc');
+}
+
+function getShoppingList(id) {
+ return db('recipe as r')
+  .join('recipe_ingredients as r_i', 'r.id', 'r_i.recipe_id')
+  .join('ingredients as i', 'i.id', 'r_i.ingredient_id')
+  .select('r.name', 'i.ingredient_name', 'r_i.quantity', 'i.unit')
+  .where('recipe_id', id);
 }
